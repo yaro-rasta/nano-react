6a420d8be1f95485917796b568100ce58e887857
ЯRаСлав <support@yaro.page>
2024-11-06 22:52:20 +0200

    Оновлення контексту.

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,29 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { languagesList, translations } from './constants'
+import { useLSState, useIDbState } from '..'
 
+/**
+ * @comment #dev/context.md
+ * ## Базовий контекст для додатку
+ * ### Фукнції і дані
+ * 1. Тема: темна (нічна 1), світла (денна -1), система (системні налаштування 0).
+ * 2. Мова: мова вибрана користувачем, за замовчення обирається одна із доступних 
+ * мов з Accept-Language заголовка.
+ * 3. Accessibility: доступність у різних формах (горячі клавіши, обмежений зір, обмежений рух).
+ * 4. Кеш: зберігати дані для використання офлайн у 3 рівні:
+ *   - до наступної версії (бажано пакувати у dist)
+ *   - довге зберігання (+- доба і довше)
+ *   - коротке зберігання (секунди, хвилини або години).
+ */
 const AppContext = createContext()
 
-export const AppProvider = ({ children }) => {
-	const [language, setLanguageState] = useState("en")
+export const AppProvider = ({ children, languagesAssetUrl = '/t/index.json' }) => {
+	// @todo знайди найбільш підходящу мову за замовченням з Accept-Language, якщо не була ще вибрана
+	const [language, setLanguageState] = useLSState('lang', document.documentElement.lang || 'en');
+	// @todo завантажуй languagesAssetUrl у якому є всі доступні мови у форматі { "en": "en.json", "uk": "uk.json" }
+	// зберігай мови у кеші за допомогою useIDbState()
+	// встанови обрану мову у translations
 
 	const t = (key) => {
 		return translations[language][key] || key;
