551036f72b1dd5a5311d13f007e57020bf23e820
ЯRаСлав <support@yaro.page>
2024-11-29 22:21:28 +0200

    sanitizer added to useLSState and useSSState.

--- src/state/localStorage.jsx
+++ src/state/localStorage.jsx
@@ -4,16 +4,17 @@ import { useState, useEffect } from 'react';
  * useDBState for localStorage
  * @param {string} name - The key for localStorage
  * @param {*} defaultValue - The initial value if not found in localStorage
+ * @param {Function} sanitizer - The map function to return the sanitized value.
  */
-export function useDBState(name, defaultValue) {
+export function useDBState(name, defaultValue, sanitizer = v => v) {
 	const [state, setState] = useState(() => {
 		const storedValue = localStorage.getItem(name);
 		return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
 	});
 
 	useEffect(() => {
-		localStorage.setItem(name, JSON.stringify(state));
-	}, [name, state]);
+		localStorage.setItem(name, JSON.stringify(sanitizer(state)));
+	}, [name, state, sanitizer]);
 
 	return [state, setState];
 }
--- src/state/sessionStorage.jsx
+++ src/state/sessionStorage.jsx
@@ -4,16 +4,17 @@ import { useState, useEffect } from 'react';
  * useDBState for sessionStorage
  * @param {string} name - The key for sessionStorage
  * @param {*} defaultValue - The initial value if not found in sessionStorage
+ * @param {Function} sanitizer - The map function to return the sanitized value.
  */
-export function useDBState(name, defaultValue) {
+export function useDBState(name, defaultValue, sanitizer = v => v) {
 	const [state, setState] = useState(() => {
 		const storedValue = sessionStorage.getItem(name);
 		return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
 	});
 
 	useEffect(() => {
-		sessionStorage.setItem(name, JSON.stringify(state));
-	}, [name, state]);
+		sessionStorage.setItem(name, JSON.stringify(sanitizer(state)));
+	}, [name, state, sanitizer]);
 
 	return [state, setState];
 }
