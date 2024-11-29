60b02aee83d616c34eac1399424d91451bf193e7
ЯRаСлав <support@yaro.page>
2024-11-29 22:28:49 +0200

    [1.1.0] - 2024-11-29

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import DocsLayout from '../components/DocsLayout';
 import MarkdownRenderer from '../components/MarkdownRenderer';
